8d5ffde4812ad73d29926e018fd68c085a61e3e8
Jonhtan Mota <jmtrs@rede.ulbra.br>
2020-03-13 09:57:17 -0300

    feat: adicionado produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loja virtual';
+
+  produtos = [new Produto("Teclado", 150.00), new Produto("Mouse", 56.00)];
+
+}
+
+
+export class Produto {
+  private nome: string;
+  private valor: Float64Array;
+
+  constructor(nome, valor) {
+    this.nome = nome;
+    this.valor = valor;
+  }
+
+  getNome() {
+    return this.nome;
+  }
+
+  getValor() {
+    return this.valor;
+  }
 }
